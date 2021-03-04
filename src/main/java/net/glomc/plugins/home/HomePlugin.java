b22c1f52aadd4513668ea7908deb3caf78feb12d
shafiahaz2478 <ythwaiba3@gmail.com>
2021-03-04 09:46:13 +0400

    Initial commit

---
+++ src/main/java/net/glomc/plugins/home/HomePlugin.java
@@ -0,0 +1,17 @@
+package net.glomc.plugins.home;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HomePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
